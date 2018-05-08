867a79f2280c667a67b339c4b701247e9434803a
ludekarts <ludekarts@gmail.com>
2018-05-08 22:09:02 +0200

    Added compiling step to UMD

---
+++ webpack.config.babel.js
@@ -0,0 +1,26 @@
+import path from "path"
+import webpack from "webpack"
+
+export default() => ({
+
+  entry: "./index.js",
+
+  output: {
+    path: path.resolve(__dirname, "./dist"),
+    filename: "index.js",
+    libraryTarget: "umd",
+    library: "rhm"
+  },
+
+  context: path.resolve(__dirname, "src"),
+
+  module: {
+    rules: [
+      {
+        test: /\.js?$/,
+        loader: "babel-loader",
+        exclude: /node_modules/
+      }
+    ]
+  }
+})
